414203c24ebc45a68a4e8849cf0bd44313bc5d7c
artiveloper <artiveloper@gmail.com>
2019-05-05 14:16:03 +0900

    매개변수를 이용한 테스트

--- src/test/java/dev/artiveloper/restapiexample/events/EventTest.java
+++ src/test/java/dev/artiveloper/restapiexample/events/EventTest.java
@@ -1,9 +1,13 @@
 package dev.artiveloper.restapiexample.events;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(JUnitParamsRunner.class)
 public class EventTest {
 
     @Test
@@ -12,4 +16,21 @@ public class EventTest {
         assertThat(event).isNotNull();
     }
 
+    @Test
+    @Parameters({
+            "0, 0, true",
+            "100, 0, false",
+            "0, 100, false"
+    })
+    public void testFree(int basePrice, int maxPrice, boolean isFree) {
+        Event event = Event.builder()
+                .basePrice(basePrice)
+                .maxPrice(maxPrice)
+                .location("강남역")
+                .build();
+
+        event.update();
+
+        assertThat(event.isFree()).isEqualTo(isFree);
+    }
 }
