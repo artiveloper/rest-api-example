9adb6b27099c03c2c21ca1bc93a89818dff695e0
artiveloper <artiveloper@gmail.com>
2019-05-05 15:25:16 +0900

    HATEOS 적용

--- src/main/java/dev/artiveloper/restapiexample/events/EventController.java
+++ src/main/java/dev/artiveloper/restapiexample/events/EventController.java
@@ -2,6 +2,7 @@ package dev.artiveloper.restapiexample.events;
 
 import lombok.AllArgsConstructor;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
@@ -35,8 +36,14 @@ public class EventController {
         Event newEvent = event.toEventEntity();
         newEvent.update();
         eventRepositoy.save(newEvent);
-        URI createdUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
-        return ResponseEntity.created(createdUri).body(newEvent);
+
+        ControllerLinkBuilder selfLinkBuilder = linkTo(EventController.class).slash(newEvent.getId());
+        URI createdUri = selfLinkBuilder.toUri();
+
+        EventResource eventResource = new EventResource(newEvent);
+        eventResource.add(linkTo(EventController.class).withRel("query-events"));
+        eventResource.add(selfLinkBuilder.withRel("update-event"));
+        return ResponseEntity.created(createdUri).body(eventResource);
     }
 
 }
