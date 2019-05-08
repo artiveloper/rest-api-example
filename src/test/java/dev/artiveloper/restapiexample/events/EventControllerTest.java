604f46883b77b613194552aca7e140eeec0b8f97
artiveloper <artiveloper@gmail.com>
2019-05-08 23:30:49 +0900

    서비스/테스트 설정파일 분리

--- src/test/java/dev/artiveloper/restapiexample/events/EventControllerTest.java
+++ src/test/java/dev/artiveloper/restapiexample/events/EventControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -24,6 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class EventControllerTest {
 
     @Autowired
